a6fd52c5b0925c84c373cb37cbfc18e1fa68606a
seb613 <seb613@op.pl>
2024-11-21 10:45:06 +0100

    fix example

--- example.c
+++ example.c
@@ -1,100 +1,104 @@
-#include <stdio.h> // Standard I/O library
-#include <system.h> // System-specific definitions
-#include "includes.h" // Additional includes for the RTOS
+#include <stdio.h>
+#include <system.h>
+#include "includes.h"
 
-typedef enum POZIOM_TEMP // Enumeration for temperature levels
+typedef enum POZIOM_TEMP
 {
-  POZIOM_0 = 0, // Level 0
-  POZIOM_1 = 2, // Level 1
-  POZIOM_2 = 4, // Level 2
-  POZIOM_3 = 8, // Level 3
-  POZIOM_4 = 16, // Level 4
-  POZIOM_5 = 32, // Level 5
-  POZIOM_6 = 64, // Level 6
-  POZIOM_7 = 128, // Level 7
-  POZIOM_8 = 256, // Level 8
-  POZIOM_9 = 512, // Level 9
-  POZIOM_10 = 1024, // Level 10
+    POZIOM_0 = 0,
+    POZIOM_1 = 2,
+    POZIOM_2 = 4,
+    POZIOM_3 = 8,
+    POZIOM_4 = 16,
+    POZIOM_5 = 32,
+    POZIOM_6 = 64,
+    POZIOM_7 = 128,
+    POZIOM_8 = 256,
+    POZIOM_9 = 512,
+    POZIOM_10 = 1024,
+
 } TEMP;
 
-#define LED0 0x00000001 // LED0 bit mask
-#define LED1 0x00000002 // LED1 bit mask
-#define LED2 0x00000004 // LED2 bit mask
-#define LED3 0x00000008 // LED3 bit mask
+#define LED0 0x00000001
+#define LED1 0x00000002
+#define LED2 0x00000004
+#define LED3 0x00000008
 
 /* Definition of Task Stacks */
-#define TASK_STACKSIZE 2048 // Stack size for tasks
-OS_STK task1_stk[TASK_STACKSIZE]; // Stack for task1
-OS_STK task2_stk[TASK_STACKSIZE]; // Stack for task2
+#define TASK_STACKSIZE 2048
+OS_STK task1_stk[TASK_STACKSIZE];
+OS_STK task2_stk[TASK_STACKSIZE];
 
-OS_EVENT *SWBox1; // Mailbox for inter-task communication
+OS_EVENT *SWBox1;
 
 /* Definition of Task Priorities */
-#define TASK1_PRIORITY 1 // Priority for task1
-#define TASK2_PRIORITY 2 // Priority for task2
+
+#define TASK1_PRIORITY 1
+#define TASK2_PRIORITY 2
 
 // alt_up_parallel_port_dev const *uchwytLEDR; = alt_up_parallel_port_open_dev("/dev/parallel_port_2");
-alt_up_parallel_port_dev *uchwytLEDR; // Handle for LED parallel port
+
+alt_up_parallel_port_dev *uchwytLEDR;
 
 /* Prints "Hello World" and sleeps for three seconds */
-void task1(void *pdata) // Task1 function
+void task1(void *pdata)
 {
-  while (1) // Infinite loop
-  {
-    printf("Hello from task1\n"); // Print message
-    int sw; // Variable to store slider value
-    int *msg; // Pointer for mailbox message
-    sw = IORD(0x80010, 0); // Read slider value from memory address
+    while (1)
+    {
+        printf("Hello from task1\n");
+        int sw;
+        int *msg;
+        sw = IORD(0x80010, 0);
 
-    printf("Slider = %d\n", sw); // Print slider value
-    OSMboxPostOpt(SWBox1, &sw, OS_POST_OPT_BROADCAST); // Post slider value to mailbox
+        printf("Slider = %d\n", sw);
+        OSMboxPostOpt(SWBox1, &sw, OS_POST_OPT_BROADCAST);
 
-    OSTimeDlyHMSM(0, 0, 1, 0); // Delay for 1 second
-  }
+        OSTimeDlyHMSM(0, 0, 1, 0); // sleep for 1 second
+    }
 }
 /* Prints "Hello World" and sleeps for three seconds */
-void task2(void *pdata) // Task2 function
+void task2(void *pdata)
 {
-  while (1) // Infinite loop
-  {
-    INT8U err; // Variable to store error code
-    printf("Hello from task2\n"); // Print message
-    int *num; // Pointer to store received value
+    while (1)
+    {
+        INT8U err;
+        printf("Hello from task2\n");
+        int *num;
 
-    num = OSMboxPend(SWBox1, 0, &err); // Wait for message from mailbox
+        num = OSMboxPend(SWBox1, 0, &err);
 
-    printf("T=%d\n", *num); // Print received value
+        printf("T=%d\n", *num);
 
-    IOWR(0x80030, 0, *num); // Write received value to memory address
-    OSTimeDlyHMSM(0, 0, 1, 0); // Delay for 1 second
-  }
+        IOWR(0x80030, 0, *num);
+        OSTimeDlyHMSM(0, 0, 1, 0); // sleep for 1 second
+    }
 }
 
 /* The main function creates two task and starts multi-tasking */
-int main(void) // Main function
+int main(void)
 {
-  SWBox1 = OSMboxCreate((void *)0); // Create mailbox
 
-  OSTaskCreateExt(task1, // Create task1
-                  NULL,
-                  (void *)&task1_stk[TASK_STACKSIZE - 1],
-                  TASK1_PRIORITY,
-                  TASK1_PRIORITY,
-                  task1_stk,
-                  TASK_STACKSIZE,
-                  NULL,
-                  0);
+    SWBox1 = OSMboxCreate((void *)0);
 
-  OSTaskCreateExt(task2, // Create task2
-                  NULL,
-                  (void *)&task2_stk[TASK_STACKSIZE - 1],
-                  TASK2_PRIORITY,
-                  TASK2_PRIORITY,
-                  task2_stk,
-                  TASK_STACKSIZE,
-                  NULL,
-                  0);
+    OSTaskCreateExt(task1,
+                    NULL,
+                    (void *)&task1_stk[TASK_STACKSIZE - 1],
+                    TASK1_PRIORITY,
+                    TASK1_PRIORITY,
+                    task1_stk,
+                    TASK_STACKSIZE,
+                    NULL,
+                    0);
 
-  OSStart(); // Start the RTOS
-  return 0; // Return from main
+    OSTaskCreateExt(task2,
+                    NULL,
+                    (void *)&task2_stk[TASK_STACKSIZE - 1],
+                    TASK2_PRIORITY,
+                    TASK2_PRIORITY,
+                    task2_stk,
+                    TASK_STACKSIZE,
+                    NULL,
+                    0);
+
+    OSStart();
+    return 0;
 }
