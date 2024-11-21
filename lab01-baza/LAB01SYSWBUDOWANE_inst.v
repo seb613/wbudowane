495b173dd54feab92c91306f1d9e07a9f01e7d6f
seb613 <seb613@op.pl>
2024-11-21 10:50:39 +0100

    deleted quartus files

--- lab01-baza/LAB01SYSWBUDOWANE_inst.v
+++
@@ -1,9 +0,0 @@
-	LAB01SYSWBUDOWANE u0 (
-		.clk_clk           (<connected-to-clk_clk>),           //        clk.clk
-		.hex_export        (<connected-to-hex_export>),        //        hex.export
-		.leds_export       (<connected-to-leds_export>),       //       leds.export
-		.pushbutton_export (<connected-to-pushbutton_export>), // pushbutton.export
-		.reset_reset_n     (<connected-to-reset_reset_n>),     //      reset.reset_n
-		.sw_sliders_export (<connected-to-sw_sliders_export>)  // sw_sliders.export
-	);
-
