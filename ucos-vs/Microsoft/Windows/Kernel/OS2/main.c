47d9369d1db2b75f382dfa7313ff5143383c8e44
seb613 <seb613@op.pl>
2024-12-02 19:13:34 +0100

    update

--- ucos-vs/Microsoft/Windows/Kernel/OS2/main.c
+++ ucos-vs/Microsoft/Windows/Kernel/OS2/main.c
@@ -1,10 +1,12 @@
 #include <windows.h>
-#include <commctrl.h> // Add this include for TBM_GETPOS
+#include <commctrl.h> 
 #include <stdio.h>
 #include <cpu.h>
-#include <lib_mem.h>
 #include <os.h>
+#include <ucos_ii.h>
+#include <ucos_ii.c>
 #include "app_cfg.h"
+#include <os_cpu.h>
 
 #define ZERO SEGA | SEGB | SEGC | SEGD | SEGE | SEGF
 #define ONE SEGB | SEGC
@@ -27,97 +29,6 @@ HWND hHexDisplay[42]; // Handles to the LED display segments
 /* Definition of Task Priorities */
 #define TASK1_PRIORITY 1
 #define TASK2_PRIORITY 2
-
-void UpdateHexDisplay()
-{
-    for (int i = 0; i < 42; i++)
-    {
-        int bit = (sw >> i) & 1;
-        SetWindowText(hHexDisplay[i], bit ? "1" : "0");
-    }
-}
-
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-    switch (uMsg)
-    {
-    case WM_COMMAND:
-        if (HIWORD(wParam) == BN_CLICKED)
-        {
-            int id = LOWORD(wParam);
-            int bit = id - 100; // Checkbox IDs start from 100
-            if (bit >= 0 && bit < 10)
-            {
-                if (SendMessage(GetDlgItem(hwnd, id), BM_GETCHECK, 0, 0) == BST_CHECKED)
-                {
-                    sw |= (1 << bit); // Set bit
-                }
-                else
-                {
-                    sw &= ~(1 << bit); // Clear bit
-                }
-                UpdateHexDisplay(); // Update the hex display
-            }
-        }
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        return 0;
-    }
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);
-}
-
-DWORD WINAPI GuiThread(LPVOID lpParam)
-{
-    HINSTANCE hInstance = GetModuleHandle(NULL);
-    WNDCLASS wc = { 0 };
-    wc.lpfnWndProc = WindowProc;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = "CheckboxWindowClass";
-    RegisterClass(&wc);
-
-    HWND hwnd = CreateWindowEx(
-        0,
-        "CheckboxWindowClass",
-        "Bit Manipulation",
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 400, 500,
-        NULL, NULL, hInstance, NULL);
-
-    char buttonLabel[16];
-
-    for (int i = 0; i < 10; i++)
-    {
-        sprintf_s(buttonLabel, sizeof(buttonLabel), "SW%d", i);
-
-        CreateWindowEx(
-            0, "BUTTON", buttonLabel,
-            WS_CHILD | WS_VISIBLE | BS_AUTOCHECKBOX,
-            10, 10 + i * 30, 260, 30,
-            hwnd, (HMENU)(100 + i), hInstance, NULL);
-    }
-
-    // Create the LED display segments
-    for (int i = 0; i < 42; i++)
-    {
-        hHexDisplay[i] = CreateWindowEx(
-            0, "STATIC", "0",
-            WS_CHILD | WS_VISIBLE | SS_CENTER,
-            10 + (i % 7) * 20, 350 + (i / 7) * 20, 20, 20,
-            hwnd, NULL, hInstance, NULL);
-    }
-
-    ShowWindow(hwnd, SW_SHOW);
-    UpdateWindow(hwnd);
-
-    MSG msg;
-    while (GetMessage(&msg, NULL, 0, 0))
-    {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
-    return 0;
-}
 
 /* Prints "Hello World" and sleeps for one second */
 void task1(void* pdata)
@@ -179,8 +90,6 @@ int main(void)
         NULL,
         0);
 
-    // Create a thread for the GUI
-    CreateThread(NULL, 0, GuiThread, NULL, 0, NULL);
 
     OSStart();                                                  /* Start multitasking (i.e. give control to uC/OS-II)   */
 
