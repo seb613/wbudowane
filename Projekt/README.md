00b370a0fe002a95a567208171907078e00e8307
seb613 <seb613@op.pl>
2024-12-03 14:59:06 +0100

    added instructions

--- Projekt/README.md
+++
@@ -1,8 +0,0 @@
-
-# ...existing code...
-
-## Building the Project
-
-1. Install vcpkg by following the instructions at https://github.com/microsoft/vcpkg.
-2. Integrate vcpkg with your CMake project by adding the following line to your `CMakeLists.txt`:
-   
---
+++ dosymulacji/software/assdasdads/obj/default/hello_ucosii.d
@@ -0,0 +1,49 @@
+obj/default/hello_ucosii.o: hello_ucosii.c ../assdasdads_bsp/system.h \
+ ../assdasdads_bsp/linker.h ../assdasdads_bsp//HAL/inc/includes.h \
+ ../assdasdads_bsp//HAL/inc/os_cpu.h \
+ ../assdasdads_bsp//HAL/inc/sys/alt_irq.h \
+ ../assdasdads_bsp//HAL/inc/nios2.h \
+ ../assdasdads_bsp//HAL/inc/alt_types.h \
+ ../assdasdads_bsp//HAL/inc/priv/alt_legacy_irq.h \
+ ../assdasdads_bsp//HAL/inc/sys/alt_irq.h \
+ ../assdasdads_bsp//UCOSII/inc/os_cfg.h \
+ ../assdasdads_bsp//HAL/inc/sys/alt_alarm.h \
+ ../assdasdads_bsp//HAL/inc/sys/alt_llist.h \
+ ../assdasdads_bsp//HAL/inc/priv/alt_alarm.h \
+ ../assdasdads_bsp//UCOSII/inc/ucos_ii.h \
+ ../assdasdads_bsp//HAL/inc/os_cpu.h ../assdasdads_bsp//HAL/inc/io.h \
+ ../assdasdads_bsp//HAL/inc/alt_types.h
+
+../assdasdads_bsp/system.h:
+
+../assdasdads_bsp/linker.h:
+
+../assdasdads_bsp//HAL/inc/includes.h:
+
+../assdasdads_bsp//HAL/inc/os_cpu.h:
+
+../assdasdads_bsp//HAL/inc/sys/alt_irq.h:
+
+../assdasdads_bsp//HAL/inc/nios2.h:
+
+../assdasdads_bsp//HAL/inc/alt_types.h:
+
+../assdasdads_bsp//HAL/inc/priv/alt_legacy_irq.h:
+
+../assdasdads_bsp//HAL/inc/sys/alt_irq.h:
+
+../assdasdads_bsp//UCOSII/inc/os_cfg.h:
+
+../assdasdads_bsp//HAL/inc/sys/alt_alarm.h:
+
+../assdasdads_bsp//HAL/inc/sys/alt_llist.h:
+
+../assdasdads_bsp//HAL/inc/priv/alt_alarm.h:
+
+../assdasdads_bsp//UCOSII/inc/ucos_ii.h:
+
+../assdasdads_bsp//HAL/inc/os_cpu.h:
+
+../assdasdads_bsp//HAL/inc/io.h:
+
+../assdasdads_bsp//HAL/inc/alt_types.h:
