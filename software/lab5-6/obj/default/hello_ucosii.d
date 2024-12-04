f02834343f2772c138894fd1f20249d3b963fdb9
seb613 <seb613@op.pl>
2024-12-04 18:45:13 +0100

    update

---
+++ software/lab5-6/obj/default/hello_ucosii.d
@@ -0,0 +1,45 @@
+obj/default/hello_ucosii.o: hello_ucosii.c ../lab5-6_bsp/system.h \
+ ../lab5-6_bsp/linker.h ../lab5-6_bsp//HAL/inc/includes.h \
+ ../lab5-6_bsp//HAL/inc/os_cpu.h ../lab5-6_bsp//HAL/inc/sys/alt_irq.h \
+ ../lab5-6_bsp//HAL/inc/nios2.h ../lab5-6_bsp//HAL/inc/alt_types.h \
+ ../lab5-6_bsp//HAL/inc/priv/alt_legacy_irq.h \
+ ../lab5-6_bsp//HAL/inc/sys/alt_irq.h ../lab5-6_bsp//UCOSII/inc/os_cfg.h \
+ ../lab5-6_bsp//HAL/inc/sys/alt_alarm.h \
+ ../lab5-6_bsp//HAL/inc/sys/alt_llist.h \
+ ../lab5-6_bsp//HAL/inc/priv/alt_alarm.h \
+ ../lab5-6_bsp//UCOSII/inc/ucos_ii.h ../lab5-6_bsp//HAL/inc/os_cpu.h \
+ ../lab5-6_bsp//HAL/inc/io.h ../lab5-6_bsp//HAL/inc/alt_types.h
+
+../lab5-6_bsp/system.h:
+
+../lab5-6_bsp/linker.h:
+
+../lab5-6_bsp//HAL/inc/includes.h:
+
+../lab5-6_bsp//HAL/inc/os_cpu.h:
+
+../lab5-6_bsp//HAL/inc/sys/alt_irq.h:
+
+../lab5-6_bsp//HAL/inc/nios2.h:
+
+../lab5-6_bsp//HAL/inc/alt_types.h:
+
+../lab5-6_bsp//HAL/inc/priv/alt_legacy_irq.h:
+
+../lab5-6_bsp//HAL/inc/sys/alt_irq.h:
+
+../lab5-6_bsp//UCOSII/inc/os_cfg.h:
+
+../lab5-6_bsp//HAL/inc/sys/alt_alarm.h:
+
+../lab5-6_bsp//HAL/inc/sys/alt_llist.h:
+
+../lab5-6_bsp//HAL/inc/priv/alt_alarm.h:
+
+../lab5-6_bsp//UCOSII/inc/ucos_ii.h:
+
+../lab5-6_bsp//HAL/inc/os_cpu.h:
+
+../lab5-6_bsp//HAL/inc/io.h:
+
+../lab5-6_bsp//HAL/inc/alt_types.h:
