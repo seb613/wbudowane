f25c93f86833241aea9fb48abe3f0991a86b40f6
seb613 <seb613@op.pl>
2024-12-12 15:49:32 +0100

    add proj

---
+++ Projekt-SystemC/Projekt-SystemC.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <systemc.h>
+
+SC_MODULE(hello_world) {
+    SC_CTOR(hello_world) {
+        SC_THREAD(main_thread);
+    }
+    void main_thread() {
+        std::cout << "Hello, SystemC!" << std::endl;
+    }
+};
+
+int sc_main(int argc, char* argv[]) {
+    hello_world hw("HW");
+    sc_start();
+    return 0;
+}
+
